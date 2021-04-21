eecfff8f28812722a7ea5fd2a20d24a80495d1cf
João Vinaud <leyenasd@gmail.com>
2021-04-21 08:37:31 -0300

    Chore: Consume data by Axios

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,6 +1,20 @@
+import { GetStaticProps } from 'next';
+import { DESTRUCTION } from 'node:dns';
+import api from '../services/api';
 
+type Episode = {
 
-export default function Home(props) {
+  id: String;
+  title: String;
+  members: String;
+
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -11,9 +25,15 @@ export default function Home(props) {
 }
 
 
-export async function getStaticProps(){
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps : GetStaticProps = async () =>{
+  const response = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  });
+  const data = response.data;
 
   return({
     props: {
