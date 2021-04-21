89bede6d4ad03b17c98ec14ec8d9ad00f5e6a104
João Vinaud <leyenasd@gmail.com>
2021-04-21 09:11:30 -0300

    Update: Format consumed API data

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,12 +1,22 @@
 import { GetStaticProps } from 'next';
-import { DESTRUCTION } from 'node:dns';
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import api from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTImeString';
 
 type Episode = {
 
   id: String;
   title: String;
+  thumbnail: String;
+  description: String;
   members: String;
+  duration: Number;
+  durationAsString: String;
+  url: String;
+  publishedAt: String;
+
 
 }
 
@@ -33,11 +43,26 @@ export const getStaticProps : GetStaticProps = async () =>{
       _order: 'desc',
     }
   });
+
   const data = response.data;
+
+  const episodes = data.map( episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+
+    };
+  })
 
   return({
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   })
