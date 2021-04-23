136d7f1f0829a5f6fcb950424e2dedcb307b22f5
João Vinaud <leyenasd@gmail.com>
2021-04-23 08:28:40 -0300

    Update: CSS for current episode on player

--- podcastr/src/components/Player/index.tsx
+++ podcastr/src/components/Player/index.tsx
@@ -2,6 +2,7 @@
 import { useContext } from "react";
 import { PlayerContext } from "../../contexts/PlayerContext";
 import styles from "./styles.module.scss";
+import Image from 'next/image'
 
 export function Player(){
 
@@ -12,14 +13,23 @@ export function Player(){
         <div className={styles.playerContainer}>
             <header>
                 <img src="/playing.svg" alt="Tocando agora"/>
-                <strong>Tocando agora {episode?.title}</strong>
+                <strong>Tocando agora </strong>
             </header>
 
-            <div className={styles.emptyPlayer}>
-                <strong>Selecione um podcast para ouvir</strong>
-            </div>
+            {episode? (
+                <div className={styles.currentEpisode}>
+                    <Image width={592} height={592} src={episode.thumbnail} objectFit="cover" />
+                    <strong>{episode.title}</strong>
+                    <span>{episode.members}</span>
+                </div>
 
-            <footer className={styles.empty}>
+            ) : (
+                <div className={styles.emptyPlayer}>
+                   <strong>Selecione um podcast para ouvir</strong>
+                </div>
+            )}
+
+            <footer className={!episode? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
